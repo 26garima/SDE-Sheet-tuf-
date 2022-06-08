7efb6ccdf5c642838b8f9d16404ea78ccf7bffcf
Garima Kushwaha <93420514+26garima@users.noreply.github.com>
2022-06-08 10:07:31 +0530

    Create Code.java

---
+++ 03. Arrays - III/01. Search in a 2D Matrix/Code.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        for(int i = 0; i < m; i++) {
+            if(target < matrix[i][n-1]) {
+                for(int j = 0; j < n; j++) {
+                    if(target == matrix[i][j])
+                        return true;
+                }
+            } else if(target == matrix[i][n-1])
+                return true;
+        }
+        return false;
+    }
+}
