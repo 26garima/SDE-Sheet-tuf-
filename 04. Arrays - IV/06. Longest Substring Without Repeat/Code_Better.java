d89a0ce89a366633865127e791ea3aae4c0f9b94
Garima Kushwaha <93420514+26garima@users.noreply.github.com>
2022-06-11 11:30:46 +0530

    Create Code_Better.java

---
+++ 04. Arrays - IV/06. Longest Substring Without Repeat/Code_Better.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0;
+        int l = 0, r = 0;
+        HashSet<Character> set = new HashSet<>();
+        int i = 0;
+        while(r < s.length()) {
+            if(!set.contains(s.charAt(r))) {
+                set.add(s.charAt(r));
+                res = Math.max(res, r-l+1);
+                r++;
+            } else {
+                while(s.charAt(l) != s.charAt(r)) {
+                    set.remove(s.charAt(l));
+                    l++;
+                }
+                set.remove(s.charAt(l));
+                l++;
+            }
+        }
+        return res;
+    }
+}
+
+// Time Complexity : O(2*n)
+// Space Complexity : O(n)
